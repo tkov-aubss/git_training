886eefb189f328c2ca16394f5dc26683cafa2bbf
tkov-aubss <au806083@uni.au.dk>
2025-09-19 10:16:48 +0000

    Editting

--- test_packages.R
+++ test_packages.R
@@ -1,6 +1,3 @@
 library(RPostgres)
-<<<<<<< HEAD
-library(tidyverse) #Often used in Data Science
-=======
-library(tidyverse) #Load many packages
->>>>>>> 8d99293ed71c55360621103109e7fb4b84f0cbc3
+library(tidyverse) #Load many packages often used in Data Science
+
